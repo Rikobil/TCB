de2b4d676f76b963105a0caf22897bf1104f13c2
Geoffrey <geogeo33@hotmail.fr>
2018-04-19 11:53:26 +0200

    styleajouté

--- functions.php
+++ functions.php
@@ -118,11 +118,11 @@ function html5blank_styles()
     wp_register_style('normalize', get_template_directory_uri() . '/normalize.css', array(), '1.0', 'all');
     wp_enqueue_style('normalize'); // Enqueue it!
 
-    wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
-    wp_enqueue_style('html5blank'); // Enqueue it!
+    wp_register_style('style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
+    wp_enqueue_style('style'); // Enqueue it!
 
-    wp_register_style('html5blank', get_template_directory_uri() . '/responsive.css', array(), '1.0', 'all');
-    wp_enqueue_style('html5blank'); // Enqueue it!
+    wp_register_style('style-home', get_template_directory_uri() . '/style-home.css', array(), '1.0', 'all');
+    wp_enqueue_style('style-home'); // Enqueue it!
 }
 
 // Register HTML5 Blank Navigation
