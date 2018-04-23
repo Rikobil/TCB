d083fac86e1b4565ec74203d707d9a8492e20c11
Louis COBIGO <louis@louiss-macbook-pro.home>
2018-04-23 18:17:17 +0200

    carousel V0.5

--- index.php
+++ index.php
@@ -12,42 +12,68 @@ get_header(); ?>
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="http://via.placeholder.com/500x300" alt="First slide">
-          <h1>
-            Réservez votre cours
-          </h1>
-          <div class="underline-title"></div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eleifend dui ac lorem auctor, in tristique nunc consectetur. Mauris hendrerit nunc vitae ullamcorper posuere.
-          </p>
-          <div class="btn btn-primary">
-            RÉSERVER
+      <div class="container">
+        <div class="row">
+          <div class="carousel-img col-xs- 12 col-md-6">
+            <img class="d-block w-100" src="http://via.placeholder.com/500x300" alt="First slide">
+          </div>
+          <div class="carousel-text col-xs- 12 col-md-6">
+            <h1>
+              Réservez </br> votre cours
+            </h1>
+            <div class="underline-title"></div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eleifend dui ac lorem auctor, in tristique nunc consectetur. Mauris hendrerit nunc vitae ullamcorper posuere.
+            </p>
+            <div class="btn btn-primary btn-carousel">
+              RÉSERVER
+            </div>
           </div>
         </div>
-        <div class="carousel-item">
-          <img class="d-block w-100" src="http://via.placeholder.com/500x300" alt="Second slide">
-          <h1>
-            Réservez votre cours
-          </h1>
-          <div class="underline-title"></div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eleifend dui ac lorem auctor, in tristique nunc consectetur. Mauris hendrerit nunc vitae ullamcorper posuere.
-          </p>
-          <div class="btn btn-primary">
-            RÉSERVER
-          </div>
+      </div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="http://via.placeholder.com/500x300" alt="Third slide">
-      <h1>
-        Réservez votre cours
-      </h1>
-      <div class="underline-title"></div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eleifend dui ac lorem auctor, in tristique nunc consectetur. Mauris hendrerit nunc vitae ullamcorper posuere.
-      </p>
-      <div class="btn btn-primary">
-        RÉSERVER
+      <div class="container">
+        <div class="container">
+          <div class="row">
+            <div class="carousel-img col-xs- 12 col-md-6">
+              <img class="d-block w-100" src="http://via.placeholder.com/500x300" alt="First slide">
+            </div>
+            <div class="carousel-text col-xs- 12 col-md-6">
+              <h1>
+                Réservez </br> votre cours
+              </h1>
+              <div class="underline-title"></div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eleifend dui ac lorem auctor, in tristique nunc consectetur. Mauris hendrerit nunc vitae ullamcorper posuere.
+              </p>
+              <div class="btn btn-primary btn-carousel">
+                RÉSERVER
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="container">
+        <div class="row">
+          <div class="carousel-img col-xs- 12 col-md-6">
+            <img class="d-block w-100" src="http://via.placeholder.com/500x300" alt="First slide">
+          </div>
+          <div class="carousel-text col-xs- 12 col-md-6">
+            <h1>
+              Réservez </br> votre cours
+            </h1>
+            <div class="underline-title"></div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eleifend dui ac lorem auctor, in tristique nunc consectetur. Mauris hendrerit nunc vitae ullamcorper posuere.
+            </p>
+            <div class="btn btn-primary btn-carousel">
+              RÉSERVER
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
