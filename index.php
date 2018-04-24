398a9640fe15da95f5b47c03a698fe63587b6baa
Louis COBIGO <louis@louiss-macbook-pro.home>
2018-04-24 17:17:54 +0200

    actu V0.5

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 Template Name: Homepage
 */
 get_header(); ?>
-
+<body>
+<div class="container-fluid">
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active rounded-circle"></li>
@@ -86,5 +87,114 @@ get_header(); ?>
     <span class="sr-only">Next</span>
   </a>
 </div>
+</div>
 
+<div class="container-fluid">
+
+  <div class="row">
+    <div class="col-1">
+    </div>
+    <div class="col-10 ctn-booking">
+      <div class="container">
+        <div class="row row-booking">
+          <div class="col-md-2">
+            <h3>Réservez </br> votre court</h3>
+          </div>
+          <div class="col-md-3">
+            <div class="form-group">
+              <select class="form-control" id="exampleFormControlSelect1">
+                <option selected>Date</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="form-group">
+              <select class="form-control" id="exampleFormControlSelect1">
+                <option selected>Heure</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="form-group">
+              <select class="form-control" id="exampleFormControlSelect1">
+                <option selected>Court</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-md-1">
+            <div class="btn btn-primary btn-booking">
+              RÉSERVER
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<div class="container ctn-actu">
+  <div class="row">
+    <div class="col-xs-8 col-md-4">
+      <h1>Actualités</h1>
+      <div class="underline-title"></div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="card-deck card-deck-actu">
+      <div class="card card-actu">
+        <img class="card-img-top" src="http://via.placeholder.com/500x300" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Tournoi de Noël</h5>
+          <div class="underline-title underline-card"></div>
+          <small class="text-muted">Last updated 3 mins ago</small>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        <div class="card-footer">
+          <a href="#" class="leftline">VOIR PLUS</a>
+        </div>
+      </div>
+      <div class="card card-actu">
+        <img class="card-img-top" src="http://via.placeholder.com/500x300" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Tournoi de Noël</h5>
+          <div class="underline-title underline-card"></div>
+          <small class="text-muted">Last updated 3 mins ago</small>
+          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        </div>
+        <div class="card-footer">
+          <a href="#" class="leftline">VOIR PLUS</a>
+        </div>
+      </div>
+      <div class="card card-actu">
+        <img class="card-img-top" src="http://via.placeholder.com/500x300/" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Tournoi de Noël</h5>
+          <div class="underline-title underline-card"></div>
+          <small class="text-muted">Last updated 3 mins ago</small>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        </div>
+        <div class="card-footer">
+          <a href="#" class="leftline">VOIR PLUS</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+</div>
+
+</body>
 <?php get_footer(); ?>
